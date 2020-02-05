68e88d65796149c2c0f96fb038f55c4a2f1c297f
ellie25v <yelizarieva.ellie@gmail.com>
2020-02-05 16:32:07 +0200

    c

--- js/script.js
+++ js/script.js
@@ -7,15 +7,14 @@ const lightboxImg = document.querySelector('.lightbox__image');
 
 function createImg(array) {
     let li;
-    const image = array.map(img => {
-        li = array.reduce((acc, img) => acc + `<li class="gallery__item"><a class="gallery__link" href=#>
+    const image = array.reduce((acc, img) => acc + `<li class="gallery__item"><a class="gallery__link" href=#>
         <img class="gallery__image" 
           src=${img.preview}
           alt=${img.description}
           data-src=${img.original}>
         </a></li>`, "")
     });
-    return gallery.insertAdjacentHTML('beforeend',li);
+    return gallery.insertAdjacentHTML('beforeend', li);
 }
 
 createImg(galleryItems);
@@ -38,31 +37,33 @@ function onClick(e) {
 function onClose(e) {
     if (!e.target.classList.contains('lightbox__image')) {
         lightbox.classList.remove("is-open");
+        lightbox.removeEventListener('click', onClose);
+        gallery.removeEventListener('keydown', onKeydown);
     }
 }
 
 
 function onKeydown(e) {
-        let index = Number(galleryItems.indexOf(galleryItems.find(img => img.original === lightboxImg.src))) - 1;
-        if (e.code === "Escape") {
-            lightbox.classList.remove("is-open");
-            lightbox.removeEventListener('click', onClose);
-            gallery.removeEventListener('keydown', onKeydown);
-        } else if (e.code === "ArrowLeft") { // left
-            if (index >= 0) {
-                const findPrevImg = Number(galleryItems.indexOf(galleryItems.find(img => img.original === lightboxImg.src))) - 1;
-                lightboxImg.src = galleryItems[findPrevImg].original;
-            } else {
-                lightboxImg.src = galleryItems[galleryItems.length - 1].original;
+    let index = Number(galleryItems.indexOf(galleryItems.find(img => img.original === lightboxImg.src))) - 1;
+    if (e.code === "Escape") {
+        lightbox.classList.remove("is-open");
+        lightbox.removeEventListener('click', onClose);
+        gallery.removeEventListener('keydown', onKeydown);
+    } else if (e.code === "ArrowLeft") { // left
+        if (index >= 0) {
+            const findPrevImg = Number(galleryItems.indexOf(galleryItems.find(img => img.original === lightboxImg.src))) - 1;
+            lightboxImg.src = galleryItems[findPrevImg].original;
+        } else {
+            lightboxImg.src = galleryItems[galleryItems.length - 1].original;
 
-            }
-        } else if (e.code === "ArrowRight") { // right
-            if (index < galleryItems.length - 2) {
-                const findNextImg = Number(galleryItems.indexOf(galleryItems.find(img => img.original === lightboxImg.src))) + 1;
-                lightboxImg.src = galleryItems[findNextImg].original;
-            } else {
-                lightboxImg.src = galleryItems[0].original
-            }
         }
-    
+    } else if (e.code === "ArrowRight") { // right
+        if (index < galleryItems.length - 2) {
+            const findNextImg = Number(galleryItems.indexOf(galleryItems.find(img => img.original === lightboxImg.src))) + 1;
+            lightboxImg.src = galleryItems[findNextImg].original;
+        } else {
+            lightboxImg.src = galleryItems[0].original
+        }
+    }
+
 }
